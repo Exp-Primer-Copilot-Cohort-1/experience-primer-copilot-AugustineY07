1e3fd376f5ed1279b7e6018351e92d980cb253a7
Augustine/Xiaoran Yuan <43792697+AugustineY07@users.noreply.github.com>
2024-01-18 14:47:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {   
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkills2 = document.getElementById("memberSkills2");
+    var memberSkills3 = document.getElementById("memberSkills3");
+
+    if (member.checked == true){
